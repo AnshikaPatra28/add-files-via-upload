a6687e103e9eefc9bac9edb0ead3b9eb2b58e501
AnshikaPatra28 <125597969+AnshikaPatra28@users.noreply.github.com>
2023-04-20 00:34:49 +0530

    Add files via upload

---
+++ words-db.js
@@ -0,0 +1,55 @@
+let wordList =[
+    {
+        word: "banana",
+        hint: "yellow fruit"
+    },
+    {
+        word:"candy",
+        hint: "sweet treat"
+    },
+    {
+         word:"github",
+        hint: "code hosting flatform"
+    },
+    {
+        word:"png",
+        hint: "a image file format"
+    },
+    {
+           word:"java",
+        hint: "programming language"  
+    },
+    {
+             word:"venice",
+        hint: "famous city of waters"
+    },
+    {
+             word:"excel",
+        hint: "microsoft product for windows"
+    },
+    {
+             word:"dance",
+        hint: "move to music"
+    },
+    {
+             word:"grape",
+        hint: "small fruit"
+    },
+    {
+             word:"honey",
+        hint: "sweet syrup"
+    },
+    {
+             word:"eagle",
+        hint: "bird of prey"
+    },
+    {
+             word:"lemon",
+        hint: "sour citrus"
+    },
+    {
+             word:"melon",
+        hint: "sweet fruit"
+    },
+    
+]
